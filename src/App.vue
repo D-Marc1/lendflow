bbf4d54a2809d0c47e271f7a133d12cbacc7ef5e
Daniel <daniel@websitebeaver.com>
2022-05-12 23:12:21 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,131 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+import products from './assets/products.json'
+
+/*
+*********** START PART 2 ************
+*/
+interface Post {
+  id: number
+  title: string
+  const: string | null
+}
+
+type NewPost = Omit<Post, 'id'> & { id?: number }
+
+// const post: NewPost = { id: 3, title: 'test', const: 'nice' }
+
+// console.log(post)
+
+/*
+*********** END PART 2 ************
+*/
+
+
+/*
+*********** START PART FUNCTION 1 ************
+*/
+const watch = (
+  obj: { [key: string | symbol]: any },
+  getCallback: (key: string | symbol, value: any) => void | undefined,
+  setCallback: (key: string | symbol, value: any) => void | undefined
+) => {
+  const proxy = new Proxy(obj, {
+      get(obj, prop) {
+        // No watch called, property doesn't exist
+        if (!Object.hasOwn(obj, prop)) return
+
+        getCallback(prop, obj[prop])
+
+        return obj[prop]
+      },
+      set(obj, prop, value) {
+        obj[prop] = value
+
+        setCallback(prop, value)
+
+        return true
+      }
+  })
+
+  return proxy
+}
+/*
+*********** END PART 1 FUNCTION ************
+*/
+
+onMounted(() => {
+  /*
+  *********** START PART 1 CALLING FUNCTION ************
+  */
+  const obj = {}
+
+  const watchedObj = watch(
+    obj,
+    (key) => console.log('property accessed: ', key),
+    (key, value) => console.log('property modified: ', key, value)
+  )
+
+  watchedObj.foo
+  watchedObj.foo = true
+  watchedObj.foo
+  /*
+  *********** END PART 1 CALLING FUNCTION ************
+  */
+
+  const outOfStockNotOnSaleUnder20: string[] = []
+
+  let mostCommonlyUsedCategory = ''
+  const categoriesArr: string[] = []
+
+  let averagePriceSaleItems = 0
+  let averagePriceSaleItemsString = ''
+  let priceCount = 0
+  let totalItemsCount = 0
+
+  let womensProductsOutOfStockByColor: { [key: string | symbol]: any } = {}
+
+  const mostFrequentItemArray = ((array: string[]) => {
+    return array.sort((a, b) =>
+      array.filter(c => c === a).length - array.filter(c => c === b).length
+    ).pop() || ''
+  })
+
+  products.forEach(({ in_stock, on_sale, price, name, categories, gender, color }) => {
+    // Get rid of dollar sign and comma
+    const priceNormalized = parseFloat(price.replace(/,/g, '').substring(1))
+
+    if (!in_stock && !on_sale && priceNormalized < 20) {
+      outOfStockNotOnSaleUnder20.push(name)
+    }
+
+    categoriesArr.push(...categories)
+
+    priceCount += parseFloat(price.substring(1))
+    totalItemsCount++
+
+    if (gender === 'female' && !in_stock) {
+      if (!Object.hasOwn(womensProductsOutOfStockByColor, color)) {
+        womensProductsOutOfStockByColor[color] = 0
+      } 
+      
+      womensProductsOutOfStockByColor[color]++
+    }
+  })
+
+  mostCommonlyUsedCategory = mostFrequentItemArray(categoriesArr)
+
+  averagePriceSaleItems = priceCount / totalItemsCount
+
+  averagePriceSaleItemsString = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(averagePriceSaleItems)
+
+  console.log(`1. Which products are out of stock, not on sale, and under $20? ${outOfStockNotOnSaleUnder20.join(', ')}`)
+  console.log(`2. What is the most commonly used category? ${mostCommonlyUsedCategory}`)
+  console.log(`3. What is the average price of sale items? ${averagePriceSaleItemsString}`)
+  console.log(`4. How many women's products are out of stock, broken down by color? ${JSON.stringify(womensProductsOutOfStockByColor)}`)
+})
+</script>
